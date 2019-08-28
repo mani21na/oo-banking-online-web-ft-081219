f2192cd3d8bbc342fbb7be2843eb46de01ee6e9b
Hanna Lee <mani21na@gmail.com>
2019-08-28 20:51:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-binding.pry
+#binding.pry
     if @status == 'complete'
       puts "Transaction was already excuted"
     elsif @sender.balance < @amount
