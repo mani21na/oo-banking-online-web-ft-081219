6def90aed6a52057f2c52f44512367cca01be953
Hanna Lee <mani21na@gmail.com>
2019-08-28 21:05:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
 #binding.pry
     if @status == 'complete'
       puts "Transaction was already excuted"
-    elsif self.sender.balance < @amount
+    elsif self.sender.balance < @amount || self.sender.status
       return "Transaction rejected. Please check your account balance."
       @status = 'rejected'
     
