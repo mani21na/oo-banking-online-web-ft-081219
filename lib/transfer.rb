1e4ec59e38fb0fd8387d6d8ea5016cca2060f49d
Hanna Lee <mani21na@gmail.com>
2019-08-28 20:42:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,6 +5,26 @@ class Transfer
     @sender = sender
     @receiver = receiver
     @amount = amount
-    @status = "pending"
+    @status = 'pending'
   end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    if @status = 'complete'
+      puts "Transaction was already excuted"
+
+    elsif @sender.balance < @amount
+      return "Transaction rejected. Please check your account balance."
+      @status = 'rejected'
+    
+    else 
+      @receiver.deposit(@amount * -1)
+      sender.deposit(@amount)
+      @status = 'complete'
+    end
+    # binding.pry
+  end  
 end
