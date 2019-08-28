ad13a93badfa1af8f9abcc1d85cbcd676bf1a24f
Hanna Lee <mani21na@gmail.com>
2019-08-28 20:03:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,17 +1,11 @@
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
-  attr_reader
+
 
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
-    @status = 'pending'
-    transfer.amount
-  end
-
-  def valid?
-    @sender.valid? && @receiver.valid?
-    #binging.pry
+    @status = "pending"
   end
 end
